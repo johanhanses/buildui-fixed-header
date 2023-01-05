551dafedc58a394b2e59e42976cd902715b9af4a
Johan Hanses <johanhanses@gmail.com>
2023-01-05 18:23:47 +0100

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,44 @@
+import { motion, useMotionTemplate, useTransform } from 'framer-motion'
+import { useBoundedScroll } from '../hooks/useBoundedScroll'
+
 export default function Header() {
+  const { scrollYBoundedProgress } = useBoundedScroll(500)
+  const scrollYBoundedProgressThrottled = useTransform(scrollYBoundedProgress, [0, 0.5, 1], [0, 0, 1])
+
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
-        <header className="flex h-20">
+        <motion.header
+          style={{
+            height: useTransform(scrollYBoundedProgressThrottled, [0, 1], [80, 50]),
+            backgroundColor: useMotionTemplate`rgb(255 255 255 / ${useTransform(
+              scrollYBoundedProgressThrottled,
+              [0, 1],
+              [1, 0.1]
+            )})`
+          }}
+          className="fixed inset-x-0 flex h-20 shadow backdrop-blur-md"
+        >
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
-            <p className="flex origin-left items-center text-xl font-semibold uppercase">
+            <motion.p
+              style={{ scale: useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0.9]) }}
+              className="flex origin-left items-center text-xl font-semibold uppercase"
+            >
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">The</span>
               <span className="-ml-1 text-2xl tracking-[-.075em]">Daily Bugle</span>
-            </p>
-            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
+            </motion.p>
+            <motion.nav
+              style={{ opacity: useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0]) }}
+              className="flex space-x-4 text-xs font-medium text-slate-400"
+            >
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
-            </nav>
+            </motion.nav>
           </div>
-        </header>
+        </motion.header>
 
-        <main className="px-8 pt-8">
+        <main className="px-8 pt-28">
           <h1 className="h-10 w-4/5 rounded bg-slate-200 text-2xl font-bold" />
           <div className="mt-8 space-y-6">
             {[...Array(2).keys()].map((i) => (
